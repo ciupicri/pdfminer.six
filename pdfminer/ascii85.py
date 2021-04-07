ea6056a4f918bfb278176be96dbff265025b2e46
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2021-04-07 18:58:56 +0300

    ascii85: use stdlib
    Use base64.a85decode from the standard library.

--- pdfminer/ascii85.py
+++ pdfminer/ascii85.py
@@ -5,41 +5,11 @@ This code is in the public domain.
 """
 
 import re
-import struct
+from base64 import a85decode
 
 
-# ascii85decode(data)
 def ascii85decode(data: bytes) -> bytes:
-    """
-    In ASCII85 encoding, every four bytes are encoded with five ASCII
-    letters, using 85 different types of characters (as 256**4 < 85**5).
-    When the length of the original bytes is not a multiple of 4, a special
-    rule is used for round up.
-
-    The Adobe's ASCII85 implementation is slightly different from
-    its original in handling the last characters.
-
-    """
-    n = b = 0
-    out = b""
-    for i in iter(data):
-        c = bytes((i,))
-        if b"!" <= c and c <= b"u":
-            n += 1
-            b = b * 85 + (ord(c) - 33)
-            if n == 5:
-                out += struct.pack(">L", b)
-                n = b = 0
-        elif c == b"z":
-            assert n == 0, str(n)
-            out += b"\0\0\0\0"
-        elif c == b"~":
-            if n:
-                for _ in range(5 - n):
-                    b = b * 85 + 84
-                out += struct.pack(">L", b)[: n - 1]
-            break
-    return out
+    return a85decode(data, adobe=True)
 
 
 # asciihexdecode(data)
